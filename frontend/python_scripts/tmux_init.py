2e5436f00e191a0f31ffe2dfbcf49a2ed30cfa9b
Thane-Patrol <mandalidis.hugh@gmail.com>
2023-02-22 11:42:47 +1100

    Fixed python script to update node-names properly

--- frontend/python_scripts/tmux_init.py
+++ frontend/python_scripts/tmux_init.py
@@ -23,13 +23,13 @@ port = str(args[2])
 
 # change it to kill the server associated with the node, not the entire server
 #kill_tmux = "tmux kill-server"
-#subprocess.run(kill_tmux, shell=True)
+#tmuxsubprocess.run(kill_tmux, shell=True)
 
-create_tmux_command = 'tmux new -s ' + node_name + " -d"
+create_tmux_command = 'tmux new -s process-spawner -d'
 subprocess.run(create_tmux_command, shell=True)
 
 # send keys to tmux session to open ttyd
-open_ttyd_cmd = 'tmux send-keys -t ' + node_name + ' "ttyd -p ' + port + ' tmux new -A -s ttyd bash" C-m'
+open_ttyd_cmd = 'tmux send-keys -t process-spawner "ttyd -p ' + port + ' tmux new -A -s ' + node_name + ' bash" C-m'
 subprocess.run(open_ttyd_cmd, shell=True)
 
 # minicom_start = 'minicom -b ' + baud_rate + ' -D /dev/ttyUSB0'
